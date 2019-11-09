3883589cc7adf9a228b14c82d786e04b84d3f9c9
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-08 21:08:34 -0500

    Commented FaGulp due to some build issues in Netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaGulp } from 'react-icons'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to the Coffee Shop <FaGulp /></p>
+    <p>Welcome to the Coffee Shop</p>
     <p>Now go build something great.</p>
   </Layout>
 )
