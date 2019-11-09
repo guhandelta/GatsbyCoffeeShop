3f50fd821a85a535c5f5e82a31c0e2eb4f009eee
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-09 15:09:57 -0500

    Added a resoponsive NavBar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from "react"
 // import { Link } from "gatsby"
 
+import NavBar from '../components/Globals/navbar'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaGulp } from 'react-icons/fa';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <NavBar />
     <h1>Hi people</h1>
-    <p>Welcome to the Coffee Shop</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to the Coffee Shop<FaGulp /></p>
+    <p>Now go build something great. </p>
   </Layout>
 )
 
