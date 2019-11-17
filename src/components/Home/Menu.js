8080fb39fa6554db780b8909aa929b630a3cf897
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-17 10:16:29 -0500

    Added the feature to filter all the drinks as per the category

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react'
 import Title from '../Globals/Title'
 import Img from 'gatsby-image'
 
+const getCategories = items =>{
+    let tempItems = items.map(item => { return item.node.category; });
+    let tempCategories = new Set(tempItems);
+    let categories = Array.from(tempCategories);
+    categories= ["all",...categories]
+    return categories;
+}
+
+
 export default class Menu extends Component {
     constructor(props){
         super(props);
         this.state = {
             items: props.items.edges,
-            beveragePack: props.items.edges
+            beverageList: props.items.edges,
+            categories: getCategories(props.items.edges)
         };
     }
+
+    handleItems = category =>{
+        let tempBeverages = [...this.state.items];
+        if(category === "all"){
+            this.setState(()=>{
+                return {beverageList:tempBeverages}
+            })
+        }else{
+            let items = tempBeverages.filter(({node})=>node.category === category);
+            this.setState(() => { return {beverageList:items} })
+        }
+    }
+
     render() {
+        
         if(this.state.items.length>0){
             return(
                 <section className="menu py-5">
@@ -20,9 +44,22 @@ export default class Menu extends Component {
                             <div className="col">
                                 <h1>Beverages Currently Available</h1>
                                 {/* Categories */}
+                                <div className="row mb-5">
+                                    <div className="col-10 mx-auto text-center">
+                                        {
+                                            this.state.categories.map((category,index) => { // This alse displays any new categories added
+                                                return( 
+                                                    <button key={index} onClick={() => {this.handleItems(category);}} className="btn btn-yellow text-capitalize m-3">
+                                                        {category}
+                                                    </button>
+                                                );
+                                            })
+                                        }
+                                    </div>
+                                </div>
                                 {/* Items */}
                                 <div className="row mb-5">
-                                    {this.state.beveragePack.map(({node}) =>{
+                                    {this.state.beverageList.map(({node}) =>{
                                         return(
                                             <div key={node.id } className="col-11 col-md-6 my-3 d-flex mx-auto">
                                                 {/* Item Image */}
@@ -32,8 +69,20 @@ export default class Menu extends Component {
                                                 {/* Item Description */}
                                                 <div className="flex-grow-1 px-3">
                                                     <div className="d-flex justify-content-between">
-                                                        <h6 className="mb-0">{node.title}</h6>
-                                                        <h6 className="mb-0">${node.price}</h6>
+                                                        <h6 className="mb-0">
+                                                            <small>
+                                                                <strong>
+                                                                    {node.title}
+                                                                </strong>
+                                                            </small>
+                                                        </h6>
+                                                        <h6 className="mb-0 text-yellow">
+                                                            <small>
+                                                                <strong>
+                                                                    ${node.price}
+                                                                </strong>
+                                                            </small>
+                                                        </h6>
                                                     </div>
                                                     <p className="text-muted">
                                                         <small>
